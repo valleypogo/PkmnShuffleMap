37fce11b0b34ff38cd0abac92aeba622004c14e0
Jesse Tao <houseestateaq@gmail.com>
2019-11-29 16:29:39 -0700

    give pokemon with only 00 forms new form numbers

--- PMSF_icons_large/rename.js
+++ PMSF_icons_large/rename.js
@@ -6,8 +6,8 @@ fs.readFile('static_masterfile.json', function(error, data) {
 		        }
 
 	    var obj = JSON.parse(data);
-	for(p=10;p<100;p++) {
-		            fs.copyFile('pokemon_icon_0' + p  + '_00.png', 'pokemon_icon_0' + p + '_'+ Object.keys(obj.pokemon[p].forms)[0] + '.png', function(err) {
+	for(p=100;p<810;p++) {
+		            fs.copyFile('pokemon_icon_' + p  + '_00.png', 'pokemon_icon_' + p + '_'+ Object.keys(obj.pokemon[p].forms)[0] + '.png', function(err) {
 				                if ( err ) console.log('ERROR: ' + err);
 				            });
 		        }
